ac849e77982a9ee59accd1641f9b2feef483fba5
Umair Ali <umairmamoor@gmail.com>
2021-09-04 05:11:52 +0500

    updated routes

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-// start by writing all the routs 
-// create a routes directory, two js files for backened and frontend
-// db is database
-
-
-//create a route and add a parameter in the req that would identifiy which note id we want to delete.
-
-// for api routes, get route, post route and delete route.
-
-// two html routes, ones gling to handle notes.html and the other one should be default
-
-
 const express = require("express");
 const path = require('path');
 const app = express();
 const PORT = 3001;
 
-//app.use(express.static("public")) --google
+app.use(express.static("public")) 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
